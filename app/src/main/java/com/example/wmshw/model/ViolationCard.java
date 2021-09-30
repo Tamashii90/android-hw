928c974863b0186ff2ce9b9311dd2eb5b5cafbe0
Tamashii90 <patoboy90@hotmail.com>
2021-10-01 01:32:58 +0300

    add more fields

--- app/src/main/java/com/example/wmshw/model/ViolationCard.java
+++ app/src/main/java/com/example/wmshw/model/ViolationCard.java
@@ -1,23 +1,16 @@
 package com.example.wmshw.model;
 
 public class ViolationCard {
+    private long id;
     private String plugedNumber;
     private String driver;
+    private String type;
     private String location;
     private boolean paid;
     private String date;
     private long tax;
 
     public ViolationCard() {
-    }
-
-    public ViolationCard(String plugedNumber, String driver, String location, boolean paid, String date, long tax) {
-        this.plugedNumber = plugedNumber;
-        this.driver = driver;
-        this.location = location;
-        this.paid = paid;
-        this.date = date;
-        this.tax = tax;
     }
 
     public String getPlugedNumber() {
@@ -68,14 +61,32 @@ public class ViolationCard {
         this.tax = tax;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     public String toString() {
-        return "{" +
-                "plugedNumber='" + plugedNumber + '\'' +
+        return "ViolationCard{" +
+                "id=" + id +
+                ", plugedNumber='" + plugedNumber + '\'' +
                 ", driver='" + driver + '\'' +
+                ", type='" + type + '\'' +
                 ", location='" + location + '\'' +
                 ", paid=" + paid +
-                ", date=" + date +
+                ", date='" + date + '\'' +
                 ", tax=" + tax +
                 '}';
     }
