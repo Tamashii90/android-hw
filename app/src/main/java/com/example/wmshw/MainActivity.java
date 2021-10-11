a7ffbe7e9bfc77df3765f7f2d5c1374ddd19e0a8
Tamashii90 <patoboy90@hotmail.com>
2021-10-11 12:04:36 +0300

    delete unnecessary components

--- app/src/main/java/com/example/wmshw/MainActivity.java
+++ app/src/main/java/com/example/wmshw/MainActivity.java
@@ -4,17 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.customview.widget.Openable;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -38,38 +33,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, UserActivity.class));
             return;
         }
-        Openable drawerLayout = findViewById(R.id.drawer_layout);
         Set<Integer> topLevelFrags = new HashSet<>();
         topLevelFrags.add(R.id.loginFragment);
         topLevelFrags.add(R.id.registerFragment);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
-        appBarConfiguration = new AppBarConfiguration.Builder(topLevelFrags).setOpenableLayout(drawerLayout).build();
+        appBarConfiguration = new AppBarConfiguration.Builder(topLevelFrags).build();
 
         setSupportActionBar(findViewById(R.id.toolbar));
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         BottomNavigationView bottomView = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomView, navController);
-
-        NavigationView sideView = findViewById(R.id.nav_view);
-        NavigationUI.setupWithNavController(sideView, navController);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.unused_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 }
