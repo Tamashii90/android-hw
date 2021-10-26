d6b885e320c5977b4d3eec18c56f4a0bd40bbed8
Tamashii90 <patoboy90@hotmail.com>
2021-10-27 01:27:25 +0300

    change violation card layout

--- app/src/main/java/com/example/wmshw/MyAdapter.java
+++ app/src/main/java/com/example/wmshw/MyAdapter.java
@@ -2,13 +2,12 @@ package com.example.wmshw;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,20 +20,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     List<ViolationCard> violationCards;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // TODO display less fields
-        TextView textPlugedNumber;
         TextView textLocation;
-        TextView textPaid;
         TextView textDate;
         TextView textTax;
         TextView textDriver;
-        RelativeLayout cardContainer;
+        ConstraintLayout cardContainer;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            textPlugedNumber = itemView.findViewById(R.id.textView_plugedNumberValue);
             textLocation = itemView.findViewById(R.id.textView_locationValue);
-            textPaid = itemView.findViewById(R.id.textView_paid);
             textDate = itemView.findViewById(R.id.textView_date);
             textTax = itemView.findViewById(R.id.textView_tax);
             textDriver = itemView.findViewById(R.id.textView_driverValue);
@@ -57,18 +51,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ViolationCard card = violationCards.get(position);
-        holder.textTax.setText(String.valueOf(card.getTax()));
-        holder.textPlugedNumber.setText(card.getPlugedNumber());
+        holder.textTax.setText("$" + String.valueOf(card.getTax()));
         holder.textDate.setText(card.getDate());
         holder.textDriver.setText(card.getDriver());
         holder.textLocation.setText(card.getLocation());
-        if (card.isPaid()) {
-            holder.textPaid.setText("Paid");
-            holder.textPaid.setTextColor(Color.parseColor("#03c457"));
-        } else {
-            holder.textPaid.setText("Not Paid");
-            holder.textPaid.setTextColor(Color.parseColor("#fb2e0e"));
-        }
         holder.cardContainer.setOnClickListener(v -> {
             SharedPreferences sharedPreferences = v.getContext().getSharedPreferences("myPrefs", Context.MODE_PRIVATE);
             String authority = sharedPreferences.getString("authority", null);
